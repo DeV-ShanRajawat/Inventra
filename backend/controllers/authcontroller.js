118798cea7fb0921196674f69de0875c42988742
Shantanu Rajawat <rajawatshantanu50@gmail.com>
2026-07-20 00:43:11 +0530

    Day 15: Added authentication structure and register API

---
+++ backend/controllers/authcontroller.js
@@ -0,0 +1,22 @@
+import User from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User Registered Successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import cors from "cors";
 import mongoose from "mongoose";
 import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/inventra")
@@ -20,9 +20,9 @@ mongoose
 
 app.use(productRoutes);
 app.use(categoryRoutes);
+app.use(authRoutes);
 
 const PORT = 5500;
-
 
 app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
